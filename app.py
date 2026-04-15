05118667c36abdf51bed460cba7f59173ec93c8f
Avotra2005 <andriniainaavotraader@gmail.com>
2026-04-15 09:10:36 +0300

    Github Actions Workflows work

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask is officially installed and running!"
+    return "Github actions workflows"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
